102c7098efff8dca1a0c09f169e44db17bd32308
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-08-06 02:51:38 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_sessions_lab_spec.rb
+++ spec/sinatra_basic_sessions_lab_spec.rb
@@ -14,15 +14,7 @@ describe App do
     end
   end
 
-  describe 'POST /checkout' do
-
-    it "displays the shopping cart contents" do 
-      visit '/'
-
-      fill_in(:item, :with => "socks")
-      click_button "submit"
-      expect(page).to have_text("socks")
-    end
+  
 
     it "sets the params in the session hash" do 
       params = {
@@ -32,7 +24,7 @@ describe App do
       expect(session.has_key?(:item)).to eq(true)
       expect(session[:item]).to eq(params[:item])
     end
-  end
+
 
   
 end
